503a36fa7ac1d9b6aa5fbf896d8403d453c34aa3
woody <johannes8873@gmail.com>
2026-01-17 15:42:32 +0800

    feat:Adding the foote

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,24 +1,29 @@
 <template>
-  <div class="navbar bg-base-100 shadow-sm">
-    <div class="flex-1">
-      <NuxtLink to="/" class="btn btn-ghost text-xl">Architecture</NuxtLink>
+  <div class="flex flex-col min-h-screen">
+    <div class="navbar bg-base-100 shadow-sm">
+      <div class="flex-1">
+        <NuxtLink to="/" class="btn btn-ghost text-xl">Architecture</NuxtLink>
+      </div>
+      <div class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li>
+            <details>
+              <summary>model</summary>
+              <ul class="bg-base-100 rounded-t-none p-2 z-30">
+                <li><NuxtLink to="/arch">Demo1</NuxtLink></li>
+                <li><NuxtLink to="/cathedral">Demo2</NuxtLink></li>
+                <li><NuxtLink to="/arch_with_env">Demo3</NuxtLink></li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="flex-none">
-      <ul class="menu menu-horizontal px-1">
-        <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li>
-          <details>
-            <summary>model</summary>
-            <ul class="bg-base-100 rounded-t-none p-2 z-30">
-              <li><NuxtLink to="/arch">Demo1</NuxtLink></li>
-              <li><NuxtLink to="/cathedral">Demo2</NuxtLink></li>
-              <li><NuxtLink to="/arch_with_env">Demo3</NuxtLink></li>
-            </ul>
-          </details>
-        </li>
-      </ul>
+    <div class="grow">
+      <slot></slot>
     </div>
+    <AppFooter />
   </div>
-  <slot></slot>
 </template>
 <script lang="ts"></script>
